82101d6e49d06e2a6b16e5f6e31ab1f790d076ea
Shohjahon <shohjahon0192@gmail.com>
2026-04-26 15:13:25 +0500

    feat: create chat page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./pages/chat/chat.component').then(m => m.ChatComponent)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
