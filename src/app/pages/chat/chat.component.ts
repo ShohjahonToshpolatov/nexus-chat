ce323bbc5a593d955eeeb01b402b386ad9dddff9
Shohjahon <shohjahon0192@gmail.com>
2026-04-26 15:14:37 +0500

    feat: build Telegram-style chat UI with sidebar and messages

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { ChatService } from '../../core/services/chat.service';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [FormsModule, NgClass],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  readonly chatService = inject(ChatService);
+  readonly messageInput = signal('');
 
+  send(): void {
+    const text = this.messageInput();
+    this.chatService.sendMessage(text);
+    this.messageInput.set('');
+  }
 }
